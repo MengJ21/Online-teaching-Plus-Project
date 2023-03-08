59b6f3b42f72c92409f0fc237886adf5c913045f
17471 <1747153981@qq.com>
2023-03-08 17:32:09 +0800

    环境搭建。

---
+++ online-teaching-plus-base/src/test/java/com/meng/OnlineTeachingPlusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTeachingPlusBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
